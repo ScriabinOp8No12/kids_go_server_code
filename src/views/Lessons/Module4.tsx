cf4794bc57711aee76317291e7188a0f06fcc148
nharwit <nharwit@gmail.com>
2024-02-15 01:13:14 -0700

    done pairing audio for module 4, had to change lots of text to match the audio

--- src/views/Lessons/Module4.tsx
+++ src/views/Lessons/Module4.tsx
@@ -21,17 +21,53 @@ import { PuzzleConfig, Goban, JGOFNumericPlayerColor } from "goban";
 import { openPopup } from "PopupDialog";
 
 class Module4 extends Content {
-    constructor() {
+    audio: HTMLAudioElement;
+
+    constructor(audioUrl: string) {
         super();
+        // Initialize the audio only if a URL is provided
+        if (audioUrl) {
+            this.audio = new Audio(audioUrl);
+        }
+    }
+
+    playAudio = () => {
+        if (this.audio) {
+            this.audio.play().catch((error) => console.error("Error playing audio:", error));
+        }
+    };
+
+    async componentDidMount() {
+        if (this.audio) {
+            try {
+                await this.audio.play();
+            } catch (error) {
+                console.error("Error playing audio:", error);
+            }
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.audio) {
+            this.audio.pause();
+            this.audio.currentTime = 0;
+        }
     }
 }
 
 class Page1 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1707852183/kids-go-server-audio-slices/slice_62_r1hj23.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button onClick={this.playAudio}>Play Audio</button>,
             <p>
-                Here is a situation that sometimes comes up. Notice that the White stone at A is in
-                atari.
+                Here's a situation that sometimes comes up. Notice that the White stone at A is in
+                atari?
             </p>,
         ];
     }
@@ -50,11 +86,17 @@ class Page1 extends Module4 {
 }
 
 class Page2 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1707854783/kids-go-server-COMBINED-audio-slices/slice_audio38_mlvvoo.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
         return [
-            <p>
-                Since Blue is capturing a stone, playing at B is allowed. But now Blue is in atari..
-            </p>,
+            <button onClick={this.playAudio}>Play Audio</button>,
+            <p>Since Blue is capturing a stone and getting a liberty, playing at B is allowed.</p>,
+            <p>But now Blue is in atari...</p>,
         ];
     }
     config(): PuzzleConfig {
@@ -72,8 +114,17 @@ class Page2 extends Module4 {
 }
 
 class Page3 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1707852173/kids-go-server-audio-slices/slice_65_gpvvns.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
-        return [<p>So White can play at A again and capture it. But now White is in atari...</p>];
+        return [
+            <button onClick={this.playAudio}>Play Audio</button>,
+            <p>So White could play at A again and capture Blue. But now White is in atari...</p>,
+        ];
     }
     config(): PuzzleConfig {
         return {
@@ -90,11 +141,20 @@ class Page3 extends Module4 {
 }
 
 class Page4 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1707854803/kids-go-server-COMBINED-audio-slices/slice_audio40_mtldvl.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button onClick={this.playAudio}>Play Audio</button>,
+            <p>So Blue could capture, but then would be in atari again...</p>,
+            <p>This could go on forever, so there is a special rule to cover it.</p>,
             <p>
-                So Blue could capture, but then would be in atari again... This could go on forever,
-                so there is a special rule to cover it.
+                You can’t repeat the position. You can’t recreate the same exact pattern of stones
+                on the board.
             </p>,
         ];
     }
@@ -113,11 +173,18 @@ class Page4 extends Module4 {
 }
 
 class Page5 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1707852250/kids-go-server-audio-slices/slice_69_dinyyb.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button onClick={this.playAudio}>Play Audio</button>,
             <p>
-                The first player to capture a stone in a position like this starts what is called a
-                ko. On this board, Blue would start a ko by capturing at 1.
+                So the first player to capture in a position like this starts what is called a "ko".
+                On this board, Blue would start a ko by capturing at 1.
             </p>,
         ];
     }
@@ -137,11 +204,18 @@ class Page5 extends Module4 {
 }
 
 class Page6 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1707852255/kids-go-server-audio-slices/slice_70_wwqk0a.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button onClick={this.playAudio}>Play Audio</button>,
             <p>
-                The rule of ko prevents a player from immediately recapturing, but it only affects
-                the first move after a capture.
+                The ko rule prevents a player from immediately recapturing. That would repeat the
+                board. But it only affects the first move after a capture.
             </p>,
         ];
     }
@@ -161,11 +235,18 @@ class Page6 extends Module4 {
 }
 
 class Page7 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1707854795/kids-go-server-COMBINED-audio-slices/slice_audio43_meba67.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button onClick={this.playAudio}>Play Audio</button>,
             <p>
-                Basically, White can play anywhere on the board except at the triangled point. So
-                perhaps White will play at 2, and then Blue might play at 3.
+                So, White can play anywhere on the board except at the triangled point. Perhaps
+                White will play at 2, and then Blue might play at say 3.
             </p>,
         ];
     }
@@ -198,14 +279,20 @@ class Page7 extends Module4 {
 }
 
 class Page8 extends Module4 {
-    text(): JSX.Element | Array<JSX.Element> {
-        return (
-            <p>
-                Since White played at 2 on the last turn, a play at A is now allowed. Now it is
-                Blue's turn to be caught by the rule of ko. Blue can play anywhere except the
-                triangled point.
-            </p>
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1707852272/kids-go-server-audio-slices/slice_73_k8hksh.wav",
         );
+    }
+
+    text(): JSX.Element | Array<JSX.Element> {
+        return [
+            <button onClick={this.playAudio}>Play Audio</button>,
+            <p>
+                Since the board has changed, a play at A is allowed. Now it’s Blue's turn to be
+                caught by the rule of ko. Blue can play anywhere except the triangled point.
+            </p>,
+        ];
     }
     config(): PuzzleConfig {
         return {
@@ -225,8 +312,17 @@ class Page8 extends Module4 {
 }
 
 class Page9 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1707852178/kids-go-server-audio-slices/slice_74_hbb6np.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
-        return <p>So perhaps Blue will play at 1 to prevent a cut by White.</p>;
+        return [
+            <button onClick={this.playAudio}>Play Audio</button>,
+            <p>So perhaps Blue will play at 1 to connect two stones and prevent a cut by White.</p>,
+        ];
     }
     config(): PuzzleConfig {
         return {
@@ -244,13 +340,21 @@ class Page9 extends Module4 {
 }
 
 class Page10 extends Module4 {
-    text(): JSX.Element | Array<JSX.Element> {
-        return (
-            <p>
-                If White feels the ko is more important, it can be filled in with 2. In this case,
-                Blue actually gets a solid position by playing at 3. So each side got something.
-            </p>
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1707852276/kids-go-server-audio-slices/slice_75_gyn0dn.wav",
         );
+    }
+
+    text(): JSX.Element | Array<JSX.Element> {
+        return [
+            <button onClick={this.playAudio}>Play Audio</button>,
+            <p>
+                If White thinks the ko is more important, it can be filled in with 2. In this case,
+                Blue actually gets a good solid position by playing at 3. So each side got
+                something.
+            </p>,
+        ];
     }
     config(): PuzzleConfig {
         return {
@@ -269,14 +373,21 @@ class Page10 extends Module4 {
 }
 
 class Page11 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1707852279/kids-go-server-audio-slices/slice_76_bcgtdu.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
-        return (
+        return [
+            <button onClick={this.playAudio}>Play Audio</button>,
             <p>
                 Sometimes winning the ko can affect the life of a whole group. If Blue can capture
-                and then fill the ko at A, the group will make two safe eyes. If White wins, all the
-                Blue stones will be prisoners!
-            </p>
-        );
+                and then fill the ko at A, the group will make two eyes. If White wins, all the Blue
+                stones can be captured!
+            </p>,
+        ];
     }
     config(): PuzzleConfig {
         return {
@@ -293,12 +404,19 @@ class Page11 extends Module4 {
 }
 
 class Page12 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1707852282/kids-go-server-audio-slices/slice_77_jfgnzq.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button onClick={this.playAudio}>Play Audio</button>,
             <p>
                 Sometimes a ko is meaningless and only affects the stone in atari. Don't worry about
-                ko too much for right now, just start playing some games on the server and you will
-                learn more as you go. When you are ready, come back and do some more lessons.
+                ko too much for right now, just start playing some games and you'll learn more as
+                you go. When you are ready, come back and do some more lessons.
             </p>,
         ];
     }
